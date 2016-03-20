fe4f34a17a2a84ca1f0ed8857fff0b09fde4bbfa
IbraDirir <khaliildirir@gmail.com>
2016-03-20 22:01:31 +0300

    prompted user first and second question and checked whether he/she got it right

--- quiz.js
+++ quiz.js
@@ -1,6 +1,15 @@
-prompt("Who was the first president of kenya?");
-var answer = "JOMO KENYATA";
-if(answer){
+var question1 = prompt("Who was the first president of kenya?");
+var answer = false;
+if(question1.toUpperCase() === "JOMO KENYATA"){
+ answer = true;
 document.write("that was the correct answer");
+}else if(!answer){
+document.write("That was not the write answer try this one? ");    
+var question2 = prompt("what is the capital city of kenya?");
+    if(question2 == "Nairobi"){
+        answer = true;
+    document.write("That's correct");
+     }
 }
+
    
