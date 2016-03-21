23e6f0e7664e792d7abf8c1a467180fb86e44fa3
IbraDirir <khaliildirir@gmail.com>
2016-03-21 12:37:55 +0300

    keeping track of the questions left

--- quiz.js
+++ quiz.js
@@ -1,15 +1,31 @@
+var questions = 5;
+var questionsLeft;
 var question1 = prompt("Who was the first president of kenya?");
 var answer = false;
+
+questionsLeft = questions - 1;
 if(question1.toUpperCase() === "JOMO KENYATA"){
- answer = true;
-document.write("that was the correct answer");
-}else if(!answer){
-document.write("That was not the write answer try this one? ");    
-var question2 = prompt("what is the capital city of kenya?");
-    if(question2 == "Nairobi"){
-        answer = true;
-    document.write("That's correct");
-     }
+
+answer = true;
+
+} else if(!answer) {  
+
+alert ("That was not the write answer try again, You're left with " + questionsLeft + " questions");
+
 }
+
+var question2 = prompt("what is the capital city of kenya?");
+     questionsLeft = questionsLeft - 1;
+    if(question2 === "Nairobi"){
+        answer = true;
+} else if(!answer) {  
+alert ("That was not the write answer try again, You're left with " + questionsLeft + " questions");
+}
+
+if(answer){
+ alert(" That's correct and You're left with " + questionsLeft + " questions");
+}
+
+
 
    
