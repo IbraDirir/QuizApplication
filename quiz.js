67d232a059781f3ed4300d91482f4f7b4c8b46d6
IbraDirir <khaliildirir@gmail.com>
2016-03-21 16:35:51 +0300

    deleted some code, written again, ranked the player

--- quiz.js
+++ quiz.js
@@ -1,31 +1,41 @@
-var questions = 5;
-var questionsLeft;
-var question1 = prompt("Who was the first president of kenya?");
-var answer = false;
-
-questionsLeft = questions - 1;
-if(question1.toUpperCase() === "JOMO KENYATA"){
-
-answer = true;
-
-} else if(!answer) {  
-
-alert ("That was not the write answer try again, You're left with " + questionsLeft + " questions");
-
+//quiz begins no answers correct
+var correct = 0;
+//ask questions
+var answer1 = prompt("Name a programming language that's also a gem");
+    if(answer1.toUpperCase() === "RUBY"){
+    correct += 1;
 }
 
-var question2 = prompt("what is the capital city of kenya?");
-     questionsLeft = questionsLeft - 1;
-    if(question2 === "Nairobi"){
-        answer = true;
-} else if(!answer) {  
-alert ("That was not the write answer try again, You're left with " + questionsLeft + " questions");
+var answer2 = prompt("Name a programming language that's also a snake");
+    if(answer2.toUpperCase() === "PYTHON"){
+        correct += 1;
+    }
+
+var answer3 = prompt("what langauge is used to style web pages?");
+if(answer3.toUpperCase() === "CSS"){
+    correct += 1;
+}
+var answer4 = prompt("what language do you use to add interactivity to a web page")
+    if(answer4.toUpperCase() === "JAVASCRIPT"){
+    correct += 1;    
+    }
+
+var answer5 = prompt("Which langauge is used to add mark up to the web pages?")
+    if(answer5.toUpperCase() === "HTML"){
+        correct += 1;
+    }
+
+//output results
+document.write("<p>You got " + correct + " out of 5 questions correct</p>");
+//output rank
+if( correct === 5 ){
+    document.write("<p><strong>You earned a gold crown</strong></p>")
+}else if (correct >= 3){
+    document.write("<p><strong>You earned a silver crown</strong></p>")
+}else if (correct >=1 ){
+    document.write("<p><strong>You earned a bronze crown</strong></p>")
 }
 
-if(answer){
- alert(" That's correct and You're left with " + questionsLeft + " questions");
-}
 
 
 
-   
